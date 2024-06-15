6e7662e6d7aba10a92b60798499ff729f1bd22f5
yvesloic5 <yvesloic58@gmail.com>
2024-06-15 15:15:27 +0300

    updated the run.py file

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, jsonify, request
+from hugchat import hugchat
+
+chatbot = hugchat.ChatBot(cookie_path="/home/yvesloic/Documents/L2-GEIT/Recherche_operationnelle/Projet/recherche-operationnelle/cookies.json")
+id = chatbot.new_conversation()
+chatbot.change_conversation(id)
+
 
 app = Flask(__name__)
 
@@ -11,9 +17,14 @@ def chat():
     msg = request.form["msg"]
     input = msg
     print(input)
+    result = chatbot.chat(input)
+    print("Response : ", str(result))
+    return str(result)
+    
+    
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 5000, debug= True)
+    app.run(host="0.0.0.0", port= 6060, debug= True)
 
 
     
